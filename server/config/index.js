dc40410e73c24606db9b74c1a4419309bf51e892
Esteban Mejia <estebanmejia277@gmail.com>
2019-08-22 00:37:04 -0500

    Dev mode and prod mode envs

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    development: {
+        name: 'Agencia de viajes [Desarrollo]'
+    },
+    production: {
+        name: 'Agencia de viajes'
+    }
+}
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const path = require('path');
+const configs = require('./config');
 
 //load router
 app.use('/', routes());
@@ -15,4 +16,19 @@ app.set('views', path.join(__dirname, './views'));
 //load statics
 app.use(express.static('public'));
 
-app.listen(4000);
+// validate if i am in dev or prod mode
+const config = configs[app.get('env')];
+
+// save the env
+app.locals.mode = config.name;
+
+
+//show actual year
+app.use((req, res, next) => {
+    const date = new Date();
+    //save value in local
+    res.locals.actualDate = date.getFullYear();
+    return next();
+});
+
+app.listen(3000);
