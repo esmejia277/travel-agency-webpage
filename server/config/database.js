3a1309157218507c4f6e22b80093a4b1e672204f
Esteban Mejía <estebanmejia277@gmail.com>
2019-09-02 10:06:24 -0500

    Envs fixed

--- server/config/database.js
+++ server/config/database.js
@@ -2,8 +2,8 @@ const Sequelize = require('sequelize');
 require('dotenv').config({ path: 'vars.env' })
 
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER , process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.BD_HOST,
+    port: process.env.BD_PORT,
     dialect: process.env.DB_DIALECT,
     define: {
         timestamps: false
